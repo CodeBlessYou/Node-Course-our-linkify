09e48bf412413165e025b3099d02bdb32900bd65
Code Bless You <codeblessyou24@gmail.com>
2025-05-20 14:26:45 +0530

    Section 13 Register a new User

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
+const userRoutes = require("./routes/users");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -14,5 +16,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
---
+++ routes/users.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/users");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Missing required form fields!", success: false });
+  }
+
+  const user = await User.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
+
+  if (user) {
+    return res.status(400).json({
+      message:
+        user.username === username
+          ? "Username is already taken!"
+          : "Email is already registered!",
+      success: false,
+    });
+  }
+
+  const hashedPass = await bcrypt.hash(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPass,
+  });
+
+  await newUser.save();
+
+  const token = generateToken({
+    _id: newUser._id,
+    username: newUser.username,
+  });
+
+  res.status(201).json(token);
+});
+
+const generateToken = (data) => {
+  return jwt.sign(data, process.env.JWT_KEY);
+};
+
+module.exports = router;
