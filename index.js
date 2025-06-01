8a15a9b9d64f34bd90af5df6d1f852b2c702283b
Code Bless You <codeblessyou24@gmail.com>
2025-06-01 13:19:49 +0530

    Section 15 Authenticate user in Socket

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const { Server } = require("socket.io");
 const http = require("http");
+const jwt = require("jsonwebtoken");
 
 const userRoutes = require("./routes/users");
 const postRoutes = require("./routes/posts");
@@ -55,23 +56,46 @@ app.use((error, req, res, next) => {
   return res.status(500).json({ message: "Internal Server Error!" });
 });
 
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+
+  if (!token) {
+    return next(new Error("Authentication error! Token required!"));
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_KEY);
+    socket.user = user;
+    console.log("Socket User", socket.user);
+
+    next();
+  } catch (error) {
+    return next(new Error("Authentication error! Token required!"));
+  }
+});
+
 io.on("connection", (socket) => {
   console.log("A user connected");
+
+  socket.emit("userData", socket.user);
 
   socket.on("joinRoom", (chatId) => {
     socket.join(chatId);
     console.log(`User ${socket.id} joined room: ${chatId}`);
   });
 
-  socket.on("typing", ({ chatId, username }) => {
-    socket.to(chatId).emit("showTyping", `${username} is typing...`);
+  socket.on("typing", ({ chatId }) => {
+    socket
+      .to(chatId)
+      .emit("showTyping", `${socket.user.username} is typing...`);
   });
 
-  socket.on("stopTyping", ({ chatId, username }) => {
-    socket.to(chatId).emit("hideTyping", username);
+  socket.on("stopTyping", ({ chatId }) => {
+    socket.to(chatId).emit("hideTyping", socket.user.username);
   });
 
-  socket.on("sendMessage", async ({ content, chatId, userId }) => {
+  socket.on("sendMessage", async ({ content, chatId }) => {
+    const userId = socket.user._id;
     if (!content) {
       socket.emit(
         "errorInSendMessage",
